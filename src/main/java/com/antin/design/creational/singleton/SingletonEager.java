509253cd6f70183fddfbd13056753e5f8493fbdd
jigong <jigong@163.com>
2017-12-25 15:03:23 +0800

    临时测试

--- src/main/java/com/antin/design/creational/singleton/SingletonEager.java
+++ src/main/java/com/antin/design/creational/singleton/SingletonEager.java
@@ -7,8 +7,10 @@ package com.antin.design.creational.singleton;
  * 饿汉式
  */
 public class SingletonEager {
+
     /* 持有私有静态实例，防止被引用，此处赋值为null，目的是实现延迟加载 */
     private static SingletonEager instance = new SingletonEager();
+
 
     /* 私有构造方法，防止被实例化 */
     private SingletonEager() {}
